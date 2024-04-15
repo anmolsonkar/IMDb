0824dc3ca266ecca46a2fcea1541649623b10ce1
AnmolSonkar <anmolsonkar742@gmail.com>
2024-04-16 01:12:15 +0530

    docker added

--- Backend/server.js
+++ Backend/server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 const routes = require('./routes/routers');
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 
 const app = express();
 const URI = process.env.MONGODB_URI;
